9e42ccca1a136d8a1adfa1a35aa95eaa3adfba1a
fadianzkaba <108704916+fadianzkaba@users.noreply.github.com>
2024-11-09 16:57:30 +1100

    Change the func name

--- calculator.go
+++ calculator.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Cal calculates the sum of val1, val2, and a random integer between 0 and 5.
-func Cal(val1 int, val2 int) int {
+func Cal1(val1 int, val2 int) int {
 	val3 := rand.Intn(6)
 	return val1 + val2 + val3
 }
